b75384c75adabde8fed7966ccb27489a4ab0e4c5
Ankur3386 <ankurdeep03386@gmail.com>
2024-11-14 18:25:03 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express from express
+import express from 'express'
 import cookieParser from "cookie-parser"
 import cors from 'cors'
 const app =express()
@@ -10,4 +10,6 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+import userRouter from './routes/user.routes.js'
+app.use("/api/v1/users",userRouter)
 export {app}
