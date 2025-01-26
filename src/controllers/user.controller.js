6c8d6ed980cd543353eb1165d120b494a8e5bbcf
Ankur3386 <ankurdeep03386@gmail.com>
2025-01-26 13:20:50 +0530

    added getWatch History end point

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -339,6 +339,50 @@ return res
 )
 
 })
+const getWatchHistory = asyncHandler(async(req,res)=>{
+const user = await User.aggregate([
+  {
+    $match:{
+      _id:new mongoose.Types.ObjectId(req.user._id)
+    }
+  },
+  {
+    $lookup:{
+      from:"videos",
+      localField:"watchHistory",
+      foreignField:"_id",
+      as:"watchHistory",
+      pipeline:[
+        {
+          $lookup:{
+            from:"users",
+            localField:"owner",
+            foreignField:"_id",
+            as:"owner",
+            pipeline:[
+              {
+                $project:{
+                  fullName:1,
+                  username:1,
+                  avatar:1
+                }
+              }
+            ]
+          }
+        },{
+          $addFields:{
+            owner:{
+              $first:"$owner"
+            }
+          }
+        }
+      ]
+    }
+  }
+])
+retuen res.status(200)
+.json(new ApiResponse(200, user[0].watchHistory,"Watch history fetched successsfully"))
+})
 export {registerUser,
   loginUser,
   logoutUser,
@@ -348,5 +392,6 @@ export {registerUser,
    updateAccountdetails,
    updateUserAvatar ,
    updateUserCoverImage,
-   getUserChannelProfile
+   getUserChannelProfile,
+   getWatchHistory
 }
